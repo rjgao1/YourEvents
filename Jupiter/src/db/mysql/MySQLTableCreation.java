bdd56101e42b4f68b1efe91a7d1443f8e8b0d9de
rjgao1 <rooney.gao@mail.utoronto.com>
2019-04-01 02:51:44 -0400

    created tables: Users, Items, Category and History

--- Jupiter/src/db/mysql/MySQLTableCreation.java
+++ Jupiter/src/db/mysql/MySQLTableCreation.java
@@ -32,6 +32,46 @@ public class MySQLTableCreation {
 			sql = "DROP TABLE IF EXISTS user";
 			statement.executeUpdate(sql);
 			
+			//Step 3: Create new tables
+			sql = "CREATE TABLE items ("
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "name VARCHAR(255),"
+					+ "rating FLOAT,"
+					+ "address VARCHAR(255),"
+					+ "image_url VARCHAR(255),"
+					+ "url VARCHAR(255),"
+					+ "distance FLOAT,"
+					+ "PRIMARY KEY (item_id)"
+					+ ")";
+			statement.executeUpdate(sql);
+
+			sql = "CREATE TABLE users ("
+					+ "user_id VARCHAR(255) NOT NULL,"
+					+ "password VARCHAR(255) NOT NULL,"
+					+ "first_name VARCHAR(255),"
+					+ "last_name VARCHAR(255),"
+					+ "PRIMARY KEY (user_id)"
+					+ ")";
+			statement.executeUpdate(sql);
+
+			sql = "CREATE TABLE categories ("
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "category VARCHAR(255) NOT NULL,"
+					+ "PRIMARY KEY (item_id, category),"
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id)"
+					+ ")";
+			statement.executeUpdate(sql);
+
+			sql = "CREATE TABLE history ("
+					+ "user_id VARCHAR(255) NOT NULL,"
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "last_favor_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+					+ "PRIMARY KEY (user_id, item_id),"
+					+ "FOREIGN KEY (user_id) REFERENCES users(user_id),"
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id)"
+					+ ")";
+			statement.executeUpdate(sql);
+			
 			conn.close();
 			System.out.println("Import done successfully.");
 		} catch(Exception e) {
